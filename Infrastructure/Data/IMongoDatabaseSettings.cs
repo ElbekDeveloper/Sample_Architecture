f66537944c96890ae72bb3f890fe72a9584eb112
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-01-24 16:46:25 +0500

    Add mongo config

---
+++ Infrastructure/Data/IMongoDatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Data
+{
+    public interface IMongoDatabaseSettings
+    {
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
---
+++ Infrastructure/Data/MongoDatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Data
+{
+    public class MongoDatabaseSettings : IMongoDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
