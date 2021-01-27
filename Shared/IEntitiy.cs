9f113117d655b5c093af615f6cb105f7f7856664
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-01-27 11:19:15 +0500

    Add generic interface for entities

---
+++ Shared/IEntitiy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    /// <summary>
+    /// Class Entity.
+    /// </summary>
+    public interface IEntitiy<TId>
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        TId Id { get; set; }
+    }
+}
